dc04baa6d3a6c2bccec7229bf234e37d2b761cd2
Ananda Kelvin <jhousin2002@gmail.com>
2023-02-17 19:38:13 +0700

    menambahkan latihan if else

---
+++ latihan_ifelse.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+namespace vin {
+    void print (string str_In) {
+     cout << str_In;}
+    void println (string str_In) {
+     cout << str_In << endl;}
+    void getstr(string & output) {
+     cin >> output;}
+    void getint(int & output) {
+     cin >> output;}
+    void getln(string & output) {
+     getline(cin, output);}
+
+}
+
+using namespace vin;
+
+int main() {
+    system("CLS");
+    
+
+    println("Kuda makanannya rumput atau ayam?");
+    print(">> ");
+
+    string jawaban;getstr(jawaban);
+
+    if (jawaban == "rumput" || jawaban == "Rumput" || jawaban == "RUMPUT") {
+        println("Jawaban anda benar, kuda makan rumput.");
+    }
+    else if (jawaban == "ayam" || jawaban == "Ayam" || jawaban == "AYAM"){
+        println("Jawaban anda salah, kuda tidak makan rumput.");
+    }
+    else {
+        print("Mohon masukkan jawaban yang sesuai pertanyaan!");
+    };
+    
+
+
+    return 0;
+}

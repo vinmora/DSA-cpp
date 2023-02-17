a7f3b9539f6c9b4c003ae5f7d068c379d66d51ff
Ananda Kelvin <jhousin2002@gmail.com>
2023-02-17 18:49:36 +0700

    modif template

--- AAATemplate.cpp
+++ AAATemplate.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+namespace myPrint {
+    void print (string str_In) {
+     cout<<str_In;
+    }
+    void println (string str_In) {
+     cout<< str_In << endl;
+    }
+    void getstr(string&output){
+     cout<<"Masukkan kata: ";
+     cin >> output;     
+    }
+    void getln(string&output){
+     cout<<"Masukkan kata: ";
+     getline(cin, output);     
+    }
+
+
+}
+
+using namespace myPrint;
 
 int main() {
     system("CLS");
     
-
-
+    
 
     return 0;
 }

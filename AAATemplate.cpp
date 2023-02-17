7156161acc34a508c33d85dc9e6b8ed941380d98
Ananda Kelvin <jhousin2002@gmail.com>
2023-02-17 19:37:36 +0700

    update personal template

--- AAATemplate.cpp
+++ AAATemplate.cpp
@@ -3,31 +3,25 @@
 
 using namespace std;
 
-namespace myPrint {
+namespace vin {
     void print (string str_In) {
-     cout<<str_In;
-    }
+     cout << str_In;}
     void println (string str_In) {
-     cout<< str_In << endl;
-    }
-    void getstr(string&output){
-     cout<<"Masukkan kata: ";
-     cin >> output;     
-    }
-    void getln(string&output){
-     cout<<"Masukkan kata: ";
-     getline(cin, output);     
-    }
-
-
+     cout << str_In << endl;}
+    void getstr(string & output) {
+     cin >> output;}
+    void getint(int & output) {
+     cin >> output;}
+    void getln(string & output) {
+     getline(cin, output);}
 }
 
-using namespace myPrint;
+using namespace vin;
 
 int main() {
     system("CLS");
     
-    
+
 
     return 0;
 }

606f17190ff589ecfebb86feaa438f1a7ff6d609
vinmora <jhousin2002@gmail.com>
2023-03-16 08:43:14 +0700

    Add lab AP-1

---
+++ AP1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+
+
+using namespace std;
+
+
+int main() {
+    system("cls");
+    int a = 19;
+    char b = 'S';
+    float c = 3.14537;
+
+    printf("%c Merupakan abjad yang ke- %d" , b , a);
+    printf("\n");
+    printf("bilangan phi = %4.2f" , c);
+    getch();
+
+    return 0;
+}

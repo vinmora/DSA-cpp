5e6cad9688a2f5bea2725d7b1a9827fc69dfbf50
vinmora <jhousin2002@gmail.com>
2023-03-16 08:47:43 +0700

    Add AP-2 lab

---
+++ AP1-2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<stdio.h>
+#include<conio.h>
+
+using namespace std;
+int main(){
+
+    int a,b,c =0 ;
+    printf("Masukkan nilai A = "); scanf("%d" , &a);
+    printf("Masukkan nilai B = "); scanf("%d" , &b);
+    c = a + b;
+    printf("Hasil Penjumlahan = %d \n",c);
+    getch();
+}

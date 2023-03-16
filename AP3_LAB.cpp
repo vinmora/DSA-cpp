ba883115b61283a2f6d37396de92dc12ae06e0fc
vinmora <jhousin2002@gmail.com>
2023-03-16 08:48:50 +0700

    add file 3 LAB AP

---
+++ AP3_LAB.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main(){
+    int a,b,c;
+    a = 2*4 + 5*4;
+    b = 20 + 20/(5+5)%2;
+    c = ((2+3) + 6*(3+(4-2))) +3;
+
+    cout << "a = " << a <<endl;
+    cout << "b = " << b <<endl;
+    cout << "c = " << c <<endl;
+}
